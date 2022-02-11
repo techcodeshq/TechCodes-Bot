4e48c595c6d6619132fbcccc6732474139c9e3b3
Dashiell Elliott <dashiell.elliott@yahoo.com>
2022-02-11 18:01:46 -0500

    make testing work?

--- deploy.js
+++ deploy.js
@@ -21,7 +21,7 @@ rest
   .catch(console.error);
 
 // Register commands globally
-if (!process.env.TESTING) {
+if (process.env.TESTING != 0) {
   rest
     .put(Routes.applicationCommands(process.env.CLIENT_ID), { body: commands })
     .then(() => console.log("Successfully registered commands globally."))
